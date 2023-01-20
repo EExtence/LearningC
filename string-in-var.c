dea44ec4c7bc204a188acd6b34df527b941c4d5d
VeinyMushroom <evanextence13243546@gmail.com>
2023-01-20 05:03:53 -0600

    found out how to store a string in a variable and did a for loop also

---
+++ string-in-var.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+   
+    char str[6] = "Hello";
+
+    /*
+    in order to print variable. Need to use format specifier first, followed by variable name
+    print formating is done inside of quotation marks
+    */
+    printf("%s\n", str);
+
+
+    /*
+    prints str variable 10 times using a for loop
+    */
+    // for(int n = 0; n < 10; ++n) {
+    //     printf("%s\n", str);
+    // };
+
+}
