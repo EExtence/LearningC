3a643a480d44a5f34445b6d0d67cafcbe97c3ed8
EExtence <evanextence13243546@gmail.com>
2023-01-31 17:33:02 -0600

    got a bit of code that 'writes over' the items in a text file

--- test.c
+++ test.c
@@ -3,32 +3,98 @@ Test bits of code or work out ideas in here
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-int main() {
-    int num; //declares an int variable called num
-    FILE *filetime; // declares a pointer of type file (i THINK it means that it points to the locations of the file in memory. FILE is just the type of variable thats being declared)
+// set some generous maximum lengths for filename size and each line in a file
+#define FILENAME_SIZE 1024
+#define MAX_LINE 2048
 
-    
-    filetime = fopen("/Users/eleanorextence/Desktop/Programming/C-example-code/LearningC/infinite-monkeys/highscore.txt", "r+");
-    
-    char funko[4] = "rssf";
-    int x = 22;
-    
+// our program works by writing all the lines of the original file to a temp 
+// file EXCEPT the line we want to replace, which we instead write a 
+// replacement line for in its place, and then we delete the original file 
+// and rename the temp file to the original file's name
+int main()
+{
+  // file pointers for the original file and temp file
+  FILE *file, *temp;
 
-    fprintf(filetime, "%d\n%s", x, funko);
-    
-    
-    
-    // int score = 25;
-    // char text[4] = "qwer";
-    
-    // fprintf(filetime, "%d\r\n%s", score, text);
-    
-    
-    
-    
+  // store the filename and temp filename
+  char filename[FILENAME_SIZE];
+  char temp_filename[FILENAME_SIZE];
+  
+  // buffer will store each line from the original file
+  char buffer[MAX_LINE];
 
+  // replace will store the line of replacement text
+  char replace[MAX_LINE];
 
-    return 0;
+  // replace_line will store the line number we wish to replace
+  int replace_line = 0;
+  
+  // ask the user to enter the filename, store it into filename
+  printf("File: ");
+  scanf("%s", filename);
+
+  // we'll make a temp file with "temp__filename" format
+  strcpy(temp_filename, "temp____");
+  strcat(temp_filename, filename);
+  
+  // ask the user for the line number to replace, store it into replace_line
+  printf("Replace Line Number: ");
+  scanf("%d", &replace_line);
+  
+  // flush stdin to get the \n char from the last scanf out, otherwise the 
+  // below fgets will 'fail' as it will immediately encounter a newline
+  fflush(stdin);
+  
+  // ask the user for the replacement line of text, store it into replace
+  printf("New Line: ");
+  fgets(replace, MAX_LINE, stdin);
+  
+  // open the original file for reading, and the temp file for writing
+  file = fopen(filename, "r");
+  temp = fopen(temp_filename, "w");
+  
+  // check if either file failed to open, if either did exit with error status
+  if (file == NULL || temp == NULL)
+  {
+    printf("Error opening files(s).\n");
+    return 1;
+  }
+  
+  // we'll keep reading the file so long as keep_reading is true
+  bool keep_reading = true;
+
+  // will keep track of the current line number we are reading from the file
+  int current_line = 1;
+
+  do 
+  {
+    // read the next line of the file into the buffer
+    fgets(buffer, MAX_LINE, file);
+    
+    // if we've reached the end of the file, stop reading
+    if (feof(file)) keep_reading = false;
+    // if the line we've reached is the one we wish to replace, write the 
+    // replacement text to this line of the temp file
+    else if (current_line == replace_line)
+      fputs(replace, temp);
+    // otherwise write this line to the temp file
+    else fputs(buffer, temp);
+    
+    // increment the current line as we will now be reading the next line
+    current_line++;
+
+  } while (keep_reading);
+  
+  // close our access to both files as we are done with them
+  fclose(file);
+  fclose(temp);
+  
+  // delete the original file, rename temp file to the original file's name
+  remove(filename);
+  rename(temp_filename, filename);
+
+  return 0;
 }
